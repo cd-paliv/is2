f1849b778ea78eea3808af33db73cd5ad5e01486
Paula Vaccaro <paulavaccaro1@gmail.com>
2023-03-23 02:30:19 -0300

    [ADD] permisos de cliente y veterinario a bd

--- src/OMD/OMDApp/models.py
+++ src/OMD/OMDApp/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser, BaseUserManager
+from django.contrib.auth.models import AbstractUser, BaseUserManager, Permission
 from django.utils.translation import gettext as _
 
 # Create your models here.
@@ -12,6 +12,16 @@ class CustomUserAccountManager(BaseUserManager):
         
         user =  self.create_user(email=email, password=password, dni=dni, first_name=first_name, last_name=last_name, birthdate=birthdate, **other_fields)
         user.set_password(password)
+        user.save()
+        return user
+    
+    def create_vet(self, email, password, dni, first_name, last_name, birthdate, **other_fields):
+        extra_fields = {}
+        extra_fields["is_staff"] = False
+        extra_fields["is_superuser"] = False
+
+        user = self.create_user(email=email, password=password, dni=dni, first_name=first_name, last_name=last_name, birthdate=birthdate, **other_fields)
+        user.user_permissions.set([Permission.objects.get(codename="Veterinario")])
         user.save()
         return user
 
@@ -27,11 +37,11 @@ class CustomUserAccountManager(BaseUserManager):
         if not email:
             raise ValueError("El email es obligatorio")
         email = self.normalize_email(email)
+        user = self.model(email=email, password=password, dni=dni, first_name=first_name, last_name=last_name, birthdate=birthdate, **other_fields)
+        user.user_permissions.set([Permission.objects.get(codename="Cliente")])
         if password is not None:
-            user = self.model(email=email, password=password, dni=dni, first_name=first_name, last_name=last_name, birthdate=birthdate, **other_fields)
             user.save()
         else:
-            user = self.model(email=email, password=password, dni=dni, first_name=first_name, last_name=last_name, birthdate=birthdate, **other_fields)
             user.set_unusable_password()
             user.save()
 
@@ -47,12 +57,28 @@ class CustomUser(AbstractUser):
     photo = models.TextField(blank=True, null=True)
     email_confirmed = models.BooleanField(default=False)
 
+    class Meta:
+        permissions = [
+            ("Cliente", "Correspondiente al rol de Cliente en la documentación"),
+        ]
+
     objects = CustomUserAccountManager()
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ["first_name", "last_name", "dni", "birthdate"]
 
     def __str__(self):
         return self.email
+
+class Veterinario(models.Model):
+    user = models.OneToOneField(CustomUser, on_delete=models.CASCADE)
+
+    class Meta:
+        permissions = [
+            ("Veterinario", "Correspondiente al rol de Veterinario en la documentación"),
+        ]
+
+    def __str__(self):
+        return f"{self.user}"
 
 class Perro(models.Model):
     name = models.CharField(max_length=50)
