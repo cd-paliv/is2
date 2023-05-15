68da98849d2598216e19c2df1e28f565795a0af6
Paula Vaccaro <paulavaccaro1@gmail.com>
2023-05-15 00:15:19 -0300

    [ADD] perfil de perro

--- src/OMD/OMD/urls.py
+++ src/OMD/OMD/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import path, re_path
 from django.views.generic.base import TemplateView
 from OMDApp.views.accounts_view import LoginView, LogOut, RegisterView, RegisterDogView, RegisterSingleDogView, ProfileView, EditProfileView, EditPasswordView
-from OMDApp.views.dogs_view import DogListView
+from OMDApp.views.dogs_view import DogListView, ProfileDogView
 
 urlpatterns = [
     path('admin/', admin.site.urls, name='admin'),
@@ -34,5 +34,7 @@ urlpatterns = [
     path('editpassword/', EditPasswordView.as_view(), name='editPassword'),
     
     # Dogs
-    path('mydogs/', DogListView.as_view(), name='dog_list'),
+    path('mydogs/', DogListView.as_view(), name='my_dogs'),
+    path('dog/<int:dog_id>/', ProfileDogView, name='dog_profile'),
+    #path('profiledog/<int:dog_id>/', EditProfileDogView, name='dog_edit_profile'),
 ]
--- src/OMD/OMDApp/views/dogs_view.py
+++ src/OMD/OMDApp/views/dogs_view.py
@@ -5,6 +5,8 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.utils.decorators import method_decorator
 from django.views.generic import ListView
 from OMDApp.decorators import email_verification_required
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import redirect, render
 from OMDApp.models import Perro
 
 
@@ -22,6 +24,7 @@ class DogListView(LoginRequiredMixin, ListView):
         dog_list = []
         for dog in dogs:
             dog_list.append({
+                'id' : dog.id,
                 'name': dog.name,
                 'breed': dog.breed,
                 'photo': dog.photo or None,
@@ -30,6 +33,12 @@ class DogListView(LoginRequiredMixin, ListView):
         context['dog_list'] = dog_list
         context['one_dog'] = len(dog_list) == 1
         return context
+    
+@login_required
+@email_verification_required
+def ProfileDogView(request, dog_id):
+    dog = Perro.objects.get(id=dog_id)
+    return render(request, 'dogs/profile.html', {'dog': dog})
 
 def calculate_age(birthDate):
     today = date.today()
